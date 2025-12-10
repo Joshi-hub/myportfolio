19c514a1dc8adcaeb0b99790a8ad9d3d82306b16
Joshi-hub <joshuaauerbach@web.de>
2025-12-10 12:56:04 +0100

    feat: add hero, about and skills sections with images

--- src/app/features/portfolio/components/skills-section/skills-section.component.ts
+++ src/app/features/portfolio/components/skills-section/skills-section.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrl: './skills-section.component.scss'
 })
 export class SkillsSectionComponent {
-
+  skills = [
+    { name: 'HTML', icon: 'assets/img/html.png' },
+    { name: 'CSS', icon: 'assets/img/css.png' },
+    { name: 'JavaScript', icon: 'assets/img/javascript.png' },
+    { name: 'TypeScript', icon: 'assets/img/typescript.png' },
+    { name: 'Angular', icon: 'assets/img/angular.png' },
+    { name: 'Firebase', icon: 'assets/img/firebase.png' },
+    { name: 'Git', icon: 'assets/img/git.png' },
+    { name: 'Material', icon: 'assets/img/md.png' },
+    { name: 'scrum', icon: 'assets/img/scrum.png' },
+    { name: 'Rest-API', icon: 'assets/img/rastapi.png' }
+  ];
 }
